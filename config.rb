469852fe6294b53c01f13997aeaaadd1210d9632
Chau Nguyen <cnguyen@hugeinc.com>
2014-04-15 11:16:57 -0400

    small updates

--- config.rb
+++ config.rb
@@ -1,11 +1,11 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "public"
+http_path = ""
 css_dir = "/public/assets/css"
 sass_dir = "_sass"
-images_dir = "/public/assets/img"
-javascripts_dir = "/public/assets/js"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
